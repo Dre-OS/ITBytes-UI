2176e2b761e85d46c5b247700300076f717a6bd1
Rozvalle <hed-fevaleros@smu.edu.ph>
2025-06-20 10:33:15 +0800

    feat: Add Login page with authentication logic and user feedback

---
+++ itbytes-ui/src/pages/Login.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/Login.css';
+import { Form, Input, Button, message } from 'antd';
+import { LockOutlined, MailOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import logo from '../assets/logo_white.webp';
+
+const apiUrl = 'http://192.168.9.5:3000/';
+
+function Login() {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = async (values) => {
+    try {
+      setLoading(true);
+
+      const response = await fetch(`${apiUrl}`);
+      const users = await response.json();
+
+      const user = users.find((user) => user.email === values.email && user.password === values.password);
+
+      if (user) {
+        sessionStorage.setItem('isAuthenticated', true);
+        sessionStorage.setItem('role', user.role);
+        message.success('Login successful!');
+        navigate('/admin');
+      } else {
+        message.error('Invalid email or password!');
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      message.error('Something went wrong. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-image">
+        <img src={logo} alt="" className="login-image-logo" />
+      </div>
+      <div className="login-form">
+        <div className='login-back'>
+          <CloseCircleOutlined
+            style={{ fontSize: '24px', cursor: 'pointer' }}
+            onClick={() => navigate('/')}
+          />
+        </div>
+        <Form
+          layout="vertical"
+          style={{ width: '100%', maxWidth: '400px' }}
+          onFinish={handleLogin}
+        >
+          <h1>Welcome Back</h1>
+          <p>Please enter your email and password to continue.</p>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'Please enter your email!' }]}
+          >
+            <Input prefix={<MailOutlined />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            className="password-field"
+            name="password"
+            rules={[{ required: true, message: 'Please enter your password!' }]}
+          >
+            <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              className="custom-button"
+              type="primary"
+              htmlType="submit"
+              style={{ width: '100%', height: '40px', borderRadius: '10px', backgroundColor: '#2C485F' }}
+              loading={loading}
+            >
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+        <div className="login-form-footer">
+          <p>No account? <a href="/register">Register Now</a></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
