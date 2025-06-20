74bcec8105aea53681018a6724d2ebdd720ee91c
Rozvalle <hed-fevaleros@smu.edu.ph>
2025-06-20 10:33:02 +0800

    feat: Refactor Navbar component to include logo and adjust menu items

--- itbytes-ui/src/components/Navbar.jsx
+++ itbytes-ui/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Menu, Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import '../styles/Navbar.css'; // Assuming you have a CSS file for styling
+import logo from '../assets/logo_small_white.png'; // Adjust the path as necessary
 
 const Navbar = () => {
     const handleAvatarClick = () => {
@@ -10,21 +11,29 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <Menu mode="horizontal" theme="dark" style={{ width: '40%' }}>
-                <Menu.Item key="home">
-                    Home
-                </Menu.Item>
-                <Menu.Item key="dashboard">
-                    Dashboard
-                </Menu.Item>
-                <Menu.Item key="profile">
-                    Profile
-                </Menu.Item>
-            </Menu>
+            <div className="navbar-content">
+                <img src={logo} alt="Logo" className="logo" style={{ height: '50px' }} />
+                <Menu
+                    mode="horizontal"
+                    theme="dark"
+                    style={{ width: '40%' }}
+                    selectedKeys={[location.pathname === '/' ? '/' : location.pathname]}
+                >
+                    <Menu.Item key="/">
+                        Home
+                    </Menu.Item>
+                    <Menu.Item key="/dashboard">
+                        Products
+                    </Menu.Item>
+                    <Menu.Item key="/profile">
+                        Profile
+                    </Menu.Item>
+                </Menu>
+            </div>
             <Avatar
                 icon={<UserOutlined />}
                 onClick={handleAvatarClick}
-                style={{ cursor: "pointer", fontSize: 24 }}
+                style={{ cursor: "pointer", fontSize: 24, backgroundColor: "#2C485F" }}
             />
         </div>
     );
