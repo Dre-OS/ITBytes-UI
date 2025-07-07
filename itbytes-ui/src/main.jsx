f04340e4a180f17b451a780fede36c886a1f8f5c
Rozvalle <elmovaleros11@gmail.com>
2025-07-07 16:42:52 +0800

    fix: correct import path for CartContext

--- itbytes-ui/src/main.jsx
+++ itbytes-ui/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { CartProvider } from './contexts/CartContext.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
