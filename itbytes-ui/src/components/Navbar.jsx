94f79ab6eaedad794ce4a7f421f1db11db083c0e
Rozvalle <elmovaleros11@gmail.com>
2025-07-09 20:25:45 +0800

    feat: enhance Navbar with category dropdown and update logo asset

--- itbytes-ui/src/components/Navbar.jsx
+++ itbytes-ui/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, Avatar, Input, Dropdown, Badge, Modal, Button } from 'antd';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { UserOutlined, ShoppingCartOutlined, HomeOutlined, ProductOutlined, ShoppingOutlined, DashboardOutlined, SearchOutlined } from '@ant-design/icons';
+import { UserOutlined, ShoppingCartOutlined, HomeOutlined, ProductOutlined, ShoppingOutlined, DashboardOutlined, SearchOutlined, AppstoreOutlined } from '@ant-design/icons';
 import '../styles/Navbar.css';
-import logo from '../assets/logo_colored.png';
+import logo from '../assets/logo_colored1.png';
 import { useCart } from "../context/CartContext"; // adjust path if needed
 import UserSession from '../utils/UserSession'; // adjust path if needed
 
@@ -54,6 +54,31 @@ const Navbar = () => {
         }
     };
 
+    const categories = [
+        "Monitor",
+        "CCTV",
+        "Computers",
+        "Smartphones",
+        "Tablets",
+        "Components",
+        "Peripherals",
+    ];
+
+    const categoryMenu = (
+        <Menu>
+            {categories.map((category) => (
+                <Menu.Item
+                    key={category}
+                    onClick={() =>
+                        navigate(`/products?category=${encodeURIComponent(category)}`)
+                    }
+                >
+                    {category}
+                </Menu.Item>
+            ))}
+        </Menu>
+    );
+
     const userMenu = (
         <Menu>
             <Menu.Item key="name" disabled>
@@ -79,64 +104,78 @@ const Navbar = () => {
         : firstName;
 
     return (
-        <div className="navbar">
-            {/* Left: Logo + Nav Links */}
-            <div className="navbar-content">
-                <img src={logo} alt="Logo" className="logo" style={{ height: '50px' }} />
-                <div className="nav-links">
-                    <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}> <HomeOutlined /> &nbsp; Home</NavLink>
-                    <NavLink to="/products" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}> <ProductOutlined /> &nbsp; Products</NavLink>
-                    <NavLink to="/orders" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}> <ShoppingOutlined /> &nbsp; Orders</NavLink>
-                    {(role && role !== "customer" && role !== "business") && (
-                        <NavLink to="/dashboard" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
-                            <DashboardOutlined /> &nbsp; Dashboard
-                        </NavLink>
+        <div>
+            <div className="navbar">
+                {/* Left: Logo + Nav Links */}
+                <div className="navbar-content">
+                    <img src={logo} alt="Logo" className="logo" style={{ height: '60px' }} />
+                </div>
+
+                {/* Middle: Search Bar */}
+                <div className="navbar-search" >
+                    <Search
+                        placeholder="Search products"
+                        onSearch={onSearch}
+                        enterButton={
+                            <Button
+                                type="primary"
+                                style={{ backgroundColor: "#2C485F", borderColor: "#2C485F" }}
+                            >
+                                <SearchOutlined style={{ color: 'white' }} />
+                            </Button>
+                        }
+                        style={{ width: '80%' }}
+                    />
+                </div>
+
+                {/* Right: Cart + Avatar or Sign In */}
+                <div className='navbar-content-right'>
+                    <div className="navbar-icon" onClick={() => navigate('/cart')}>
+                        <Badge count={uniqueItems} size="default" offset={[4, -4]} color="#2C485F">
+                            <ShoppingCartOutlined style={{ fontSize: 24, color: '#2C485F' }} />
+                        </Badge>
+                        <p style={{ color: "#2C485F", margin: 0 }}>Cart</p>
+                    </div>
+                    <div style={{ width: '1px', height: '30px', backgroundColor: '#2C485F', opacity: 0.2 }} />
+                    {isAuthenticated ? (
+                        <Dropdown overlay={userMenu} placement="bottomRight" arrow>
+                            <div className='navbar-icon' style={{ cursor: "pointer" }}>
+                                <Avatar style={{ backgroundColor: '#2C485F', fontWeight: '' }}>
+                                    {avatarText}
+                                </Avatar>
+
+                                <p style={{ color: "#2C485F" }}>{firstName}</p>
+                            </div>
+                        </Dropdown>
+                    ) : (
+                        <div className='navbar-icon' onClick={handleAvatarClick}>
+                            <UserOutlined style={{ fontSize: 24, color: '#2C485F' }} />
+                            <p style={{ color: "#2C485F" }}>Sign In</p>
+                        </div>
                     )}
                 </div>
             </div>
-
-            {/* Middle: Search Bar */}
-            <div className="navbar-search">
-                <Search
-                    placeholder="Search products"
-                    onSearch={onSearch}
-                    enterButton={
-                        <Button
-                            type="primary"
-                            style={{ backgroundColor: "#2C485F", borderColor: "#2C485F" }}
-                        >
-                            <SearchOutlined style={{ color: 'white' }} />
-                        </Button>
-                    }
-                    style={{ width: '100%' }}
-                />
-            </div>
-
-            {/* Right: Cart + Avatar or Sign In */}
-            <div className='navbar-content-right'>
-                <div className="navbar-icon" onClick={() => navigate('/cart')}>
-                    <Badge count={uniqueItems} size="default" offset={[4, -4]} color="#2C485F">
-                        <ShoppingCartOutlined style={{ fontSize: 24, color: '#2C485F' }} />
-                    </Badge>
-                    <p style={{ color: "#2C485F", margin: 0 }}>Cart</p>
-                </div>
-                <div style={{ width: '1px', height: '30px', backgroundColor: '#2C485F', opacity: 0.2 }} />
-                {isAuthenticated ? (
-                    <Dropdown overlay={userMenu} placement="bottomRight" arrow>
-                        <div className='navbar-icon' style={{ cursor: "pointer" }}>
-                            <Avatar style={{ backgroundColor: '#2C485F', fontWeight: '' }}>
-                                {avatarText}
-                            </Avatar>
-
-                            <p style={{ color: "#2C485F" }}>{firstName}</p>
-                        </div>
-                    </Dropdown>
-                ) : (
-                    <div className='navbar-icon' onClick={handleAvatarClick}>
-                        <UserOutlined style={{ fontSize: 24, color: '#2C485F' }} />
-                        <p style={{ color: "#2C485F" }}>Sign In</p>
+            <div className='navbar-bottom'>
+                <div style={{ display: 'flex', flexDirection: 'row', gap: '20px', alignItems: 'center', width: '100%' }}>
+                    <div style={{ marginRight: 50 }}>
+                        <Dropdown overlay={categoryMenu} trigger={['click']}>
+                            <span className="nav-link" style={{ cursor: 'pointer' }}>
+                                <AppstoreOutlined /> &nbsp; Shop by Category
+                            </span>
+                        </Dropdown>
                     </div>
-                )}
+                    <div style={{ width: '1px', height: '30px', backgroundColor: '#2C485F', opacity: 0.2 }} />
+                    <div className="nav-links">
+                        <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}> <HomeOutlined /> &nbsp; Home</NavLink>
+                        <NavLink to="/products" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}> <ProductOutlined /> &nbsp; Products</NavLink>
+                        <NavLink to="/orders" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}> <ShoppingOutlined /> &nbsp; Orders</NavLink>
+                        {(role && role !== "customer" && role !== "business") && (
+                            <NavLink to="/dashboard" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                                <DashboardOutlined /> &nbsp; Dashboard
+                            </NavLink>
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
     );
