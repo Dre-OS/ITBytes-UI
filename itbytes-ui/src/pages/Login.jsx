d4ea2dfe3fee56fcac83caa2c3e95aa8beca45f8
Rozvalle <hed-fevaleros@smu.edu.ph>
2025-07-14 10:41:17 +0800

    fix: reduce transition duration for login animations

--- itbytes-ui/src/pages/Login.jsx
+++ itbytes-ui/src/pages/Login.jsx
@@ -46,7 +46,7 @@ function Login() {
         initial={{ x: '-100%', opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         exit={{ x: '-100%', opacity: 0 }}
-        transition={{ duration: 1.5, ease: 'easeInOut' }}
+        transition={{ duration: .8, ease: 'easeInOut' }}
       >
         <img src={logo} alt="" className="login-image-logo" />
         <div style={{ textAlign: 'center', width: '60%', marginTop: '-30px' }}>
@@ -60,7 +60,7 @@ function Login() {
         initial={{ x: '100%', opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         exit={{ x: '100%', opacity: 0 }}
-        transition={{ duration: 1.5, ease: 'easeInOut', delay: 0.1 }}
+        transition={{ duration: .8, ease: 'easeInOut', delay: 0.1 }}
       >
         <Form
           layout="vertical"
